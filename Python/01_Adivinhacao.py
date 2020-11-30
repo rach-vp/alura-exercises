488731a5ee0b7a06f1251a4154ebd122fa124c62
rach-vp <picanco.raquel@gmail.com>
2020-11-30 10:33:45 -0400

    Muda testes para booleanos e organiza ifs

--- Python/01_Adivinhacao.py
+++ Python/01_Adivinhacao.py
@@ -7,12 +7,17 @@ secret_num = 13
 guess = input('Digite um número:\n')
 print(f'Você digitou o número {guess} \n')
 
-if (int(guess) == secret_num):
+right = int(guess) == secret_num
+bigger = int(guess) > secret_num
+lower = int(guess) < secret_num
+
+if (right):
   print('Você acertou o número secreto!\n')
-elif (int(guess) > secret_num):
-  print('Que pena! Seu palpite é maior que o númeor secreto.\n')
 else:
-  print('Que pena! Seu palpite é menor que o númeor secreto.\n')
+  if (bigger):
+    print('Que pena! Seu palpite é maior que o númeor secreto.\n')
+  elif (lower):
+    print('Que pena! Seu palpite é menor que o númeor secreto.\n')
 
 print('***************************************')
 print('*            GAME OVER!               *')
