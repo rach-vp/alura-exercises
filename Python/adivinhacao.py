90cfe92879996985328d74d7fa55c69dea4e2750
rach-vp <picanco.raquel@gmail.com>
2020-11-30 09:50:17 -0400

    Adiciona input de número e comparação com secret

--- Python/adivinhacao.py
+++ Python/adivinhacao.py
@@ -1,1 +1,17 @@
-print("Bem vindo ao jogo de Adivinhação!")
+print('***************************************')
+print('*  Bem vindo ao jogo de Adivinhação!  *')
+print('***************************************\n')
+
+secret_num = 13
+
+guess = input('Digite um número:\n')
+print(f'Você digitou o número {guess} \n')
+
+if (int(guess) == secret_num):
+  print('Você acertou o número secreto!\n')
+else:
+  print('Que pena! Você errou o númeor secreto.\n')
+
+print('***************************************')
+print('*            GAME OVER!               *')
+print('***************************************\n')
