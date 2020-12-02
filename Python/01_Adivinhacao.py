fa900ca30cc4eaf4e9fa19460d5a745177b6a991
rach-vp <picanco.raquel@gmail.com>
2020-12-02 10:24:17 -0400

    Troca o laço while por for

--- Python/01_Adivinhacao.py
+++ Python/01_Adivinhacao.py
@@ -6,8 +6,7 @@ secret_num = 13
 
 attempt_total = input('Digite o número de tentativas:\n')
 print(f'Você tem {attempt_total} tentativa(s).\n')
-attempt_num = 1
-while (attempt_num <= int(attempt_total)):
+for attempt_num in range(1, int(attempt_total) + 1):
   print(f'Tentativa {attempt_num} de {attempt_total}.\n')
   guess = input('Digite um número:\n')
   right = int(guess) == secret_num
@@ -21,7 +20,6 @@ while (attempt_num <= int(attempt_total)):
       print('Que pena! Seu palpite é maior que o número secreto.\n')
     elif (lower):
       print('Que pena! Seu palpite é menor que o número secreto.\n')
-  attempt_num += 1
 
 print('***************************************')
 print('*            GAME OVER!               *')
