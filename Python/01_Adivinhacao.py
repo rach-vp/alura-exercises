a2cc9a5112dc8a31027983abde5d33a32abec50b
rach-vp <picanco.raquel@gmail.com>
2020-12-02 10:28:36 -0400

    Limita intevalo de input do número palpite

--- Python/01_Adivinhacao.py
+++ Python/01_Adivinhacao.py
@@ -8,7 +8,10 @@ attempt_total = input('Digite o número de tentativas:\n')
 print(f'Você tem {attempt_total} tentativa(s).\n')
 for attempt_num in range(1, int(attempt_total) + 1):
   print(f'Tentativa {attempt_num} de {attempt_total}.\n')
-  guess = input('Digite um número:\n')
+  guess = input('Digite um número de 1 a 100:\n')
+  if (int(guess) < 1 or int(guess) > 100):
+    print('Número digitado está fora do intervalo permitido.\n')
+    continue
   right = int(guess) == secret_num
   higher = int(guess) > secret_num
   lower = int(guess) < secret_num
