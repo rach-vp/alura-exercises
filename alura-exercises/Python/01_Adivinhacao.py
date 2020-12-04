510c5a7ebd83f10732882bd96a1d7a3b3145f334
rach-vp <picanco.raquel@gmail.com>
2020-12-04 09:34:16 -0400

    Adiciona sistema de pontuação

--- alura-exercises/Python/01_Adivinhacao.py
+++ alura-exercises/Python/01_Adivinhacao.py
@@ -6,6 +6,7 @@ print('***************************************\n')
 
 secret_num = round(random.randrange(1, 101))
 right_guess = False
+score = 1000
 
 attempt_total = input('Digite o número de tentativas:\n')
 print(f'Você tem {attempt_total} tentativa(s).\n')
@@ -24,12 +25,15 @@ for attempt_num in range(1, int(attempt_total) + 1):
     break
   elif (higher):
     print('Que pena! Seu palpite é maior que o número secreto.\n')
+    score -= abs(secret_num - int(guess))
   else:
     print('Que pena! Seu palpite é menor que o número secreto.\n')
+    score -= abs(secret_num - int(guess))
 if (right_guess == False):
+  score -= abs(secret_num - int(guess) - 500)
   print('Sua tentativas esgotaram.')
   print(f'O número secreto era {secret_num}.\n')
-
+print(f'Sua pontuação foi {score}.\n')
 print('***************************************')
 print('*            GAME OVER!               *')
 print('***************************************\n')
