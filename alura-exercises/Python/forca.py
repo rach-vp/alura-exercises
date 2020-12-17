f517a0709f66f5cecd11c17b79199898e0f9a3e9
rach-vp <picanco.raquel@gmail.com>
2020-12-17 08:43:36 -0400

    Corrige bugs e refatora o código

--- alura-exercises/Python/forca.py
+++ alura-exercises/Python/forca.py
@@ -3,7 +3,7 @@
 import random
 
 def getRandomWord():
-  words_bank = open('liguagens.txt', 'r')
+  words_bank = open("liguagens.txt", "r")
   words = []
   for line in words_bank:
     words.append(line.strip().upper())
@@ -11,11 +11,24 @@ def getRandomWord():
   random_index = random.randrange(0, len(words))
   return words[random_index]
 
+def checkedInput():
+  inputed_letter = input("\nDigite uma letra:\n").upper()
+  while (inputed_letter == ""):
+    print("Input vazio. Digite novamente.")
+    inputed_letter = input("\nDigite uma letra:\n").upper()
+  return inputed_letter
+
 def addRightGuess(guessed_letter, secret_word, guessing_string):
   for letter in range(0, len(secret_word), 1):
     if (secret_word[letter] == guessed_letter.strip().upper()):
       guessing_string[letter] = guessed_letter
   return guessing_string
+
+def checkResult(hanged, right, secret_word):
+  if (right):
+    print_winner_screen(secret_word)
+  elif (hanged):
+    print_loser_sreen(secret_word)
 
 def draw_hanging(miss):
     print("\n  _______     ")
@@ -66,7 +79,8 @@ def draw_hanging(miss):
     print(" |            ")
     print("_|___         \n")
 
-def print_winner_screen():
+def print_winner_screen(secret_word):
+  print(f"\nA palavra secreta era {secret_word}.")
   print("Parabéns, você ganhou!")
   print("       ___________      ")
   print("      '._==_==_=_.'     ")
@@ -74,13 +88,13 @@ def print_winner_screen():
   print("     | (|:.     |) |    ")
   print("      '-|:.     |-'     ")
   print("        \\::.    /      ")
-  print("         '::. .'        ")
+  print("          '::. .'        ")
   print("           ) (          ")
   print("         _.' '._        ")
   print("        '-------'       ")
 
 def print_loser_sreen(secret_word):
-  print("Você foi enforcado!")
+  print("\nVocê foi enforcado!")
   print(f"A palavra era {secret_word}")
   print("    _______________         ")
   print("   /               \       ")
@@ -100,30 +114,27 @@ def print_loser_sreen(secret_word):
   print("       \_______/           ")
 
 def play():
-  print('\n***************************************')
-  print('*     Bem vindo ao jogo da Forca!     *')
-  print('***************************************\n')
+  print("\n***************************************")
+  print("*     Bem vindo ao jogo da Forca!     *")
+  print("***************************************\n")
   hanged = False
   miss = 0
   right = False
   secret_word = getRandomWord()
-  guessing_string = ['_' for letter in secret_word]
+  guessing_string = ["_" for letter in secret_word]
   while (not hanged and not right):
-    print(' '.join(guessing_string))
-    guessed_letter = input('\nDigite uma letra:\n').upper()
+    print(" ".join(guessing_string))
+    guessed_letter = checkedInput()
     if (guessed_letter in secret_word):
       guessing_string = addRightGuess(guessed_letter, secret_word, guessing_string)
     else:
       miss += 1
       draw_hanging(miss)
-    if ('_' not in guessing_string):
-      right = True
-      print_winner_screen()
-    elif (miss == 7):
-      hanged = True
-      print_loser_sreen(secret_word)
-  print('\n***************************************')
-  print('*            GAME OVER!               *')
-  print('***************************************\n')
+    hanged = miss == 7
+    right = "_" not in guessing_string
+  checkResult(hanged, right, secret_word)
+  print("\n***************************************")
+  print("*            GAME OVER!               *")
+  print("***************************************\n")
 
-if __name__ == '__main__': play()
+if __name__ == "__main__": play()
