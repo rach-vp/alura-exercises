7d97947093fd3728e28fe114ef70111edf11023b
rach-vp <picanco.raquel@gmail.com>
2020-12-17 09:29:11 -0400

    Refatoraçào do ojogo da adivinhação

--- alura-exercises/Python/adivinhacao.py
+++ alura-exercises/Python/adivinhacao.py
@@ -1,41 +1,86 @@
 import random
 
+def checkedInput():
+  guess = input("Digite um número de 1 a 100:\n")
+  while (int(guess) < 1 or int(guess) > 100):
+    print("Número digitado está fora do intervalo permitido.\n")
+    guess = input("Digite um número de 1 a 100:\n")
+  while (guess == ""):
+    print("Input vazio.\n")
+    guess = input("Digite um número de 1 a 100:\n")
+  return guess
+
+def compareGuess(higher):
+  if (higher):
+    print("Que pena! Seu palpite é maior que o número secreto.\n")
+  else:
+    print("Que pena! Seu palpite é menor que o número secreto.\n")
+
+def updateScore(score, secret_num, guess):
+  score -= abs(secret_num - int(guess))
+  return score
+
+def print_winner_screen():
+  print("Parabéns, você ganhou!")
+  print("       ___________      ")
+  print("      '._==_==_=_.'     ")
+  print("      .-\\:      /-.    ")
+  print("     | (|:.     |) |    ")
+  print("      '-|:.     |-'     ")
+  print("        \\::.    /      ")
+  print("          '::. .'        ")
+  print("           ) (          ")
+  print("         _.' '._        ")
+  print("        '-------'       ")
+
+def print_loser_sreen(secret_num):
+  print("\nSua tentativas se esgotaram!")
+  print(f"O número secreto era {secret_num}.")
+  print("    _______________         ")
+  print("   /               \       ")
+  print("  /                 \      ")
+  print("//                   \/\  ")
+  print("\|   XXXX     XXXX   | /   ")
+  print(" |   XXXX     XXXX   |/     ")
+  print(" |   XXX       XXX   |      ")
+  print(" |                   |      ")
+  print(" \__      XXX      __/     ")
+  print("   |\     XXX     /|       ")
+  print("   | |           | |        ")
+  print("   | I I I I I I I |        ")
+  print("   |  I I I I I I  |        ")
+  print("   \_             _/       ")
+  print("     \_         _/         ")
+  print("       \_______/           ")
+
 def play():
-  print('\n***************************************')
-  print('*  Bem vindo ao jogo de Adivinhação!  *')
-  print('***************************************\n')
+  print("\n***************************************")
+  print("*  Bem vindo ao jogo de Adivinhação!  *")
+  print("***************************************\n")
 
+  attempt_num = 1
   secret_num = round(random.randrange(1, 101))
-  right_guess = False
+  right = False
   score = 1000
-
-  attempt_total = input('Digite o número de tentativas:\n')
-  print(f'Você tem {attempt_total} tentativa(s).\n')
-  for attempt_num in range(1, int(attempt_total) + 1):
-    print(f'Tentativa {attempt_num} de {attempt_total}.\n')
-    guess = input('Digite um número de 1 a 100:\n')
-    if (int(guess) < 1 or int(guess) > 100):
-      print('Número digitado está fora do intervalo permitido.\n')
-      continue
+  attempt_total = int(input("Digite o número de tentativas:\n"))
+  print(f"Você tem {attempt_total} tentativa(s).\n")
+  while (not right and attempt_num <= attempt_total):
+    print(f"Tentativa {attempt_num} de {attempt_total}.\n")
+    guess = checkedInput()
     right = int(guess) == secret_num
     higher = int(guess) > secret_num
     if (right):
-      print('Você acertou o número secreto!\n')
-      right_guess = True
-      break
-    elif (higher):
-      print('Que pena! Seu palpite é maior que o número secreto.\n')
-      score -= abs(secret_num - int(guess))
+      print_winner_screen()
     else:
-      print('Que pena! Seu palpite é menor que o número secreto.\n')
-      score -= abs(secret_num - int(guess))
-  if (right_guess == False):
-    score -= abs(secret_num - int(guess) - 500)
-    print('Sua tentativas esgotaram.')
-    print(f'O número secreto era {secret_num}.\n')
-  print(f'Sua pontuação foi {score}.\n')
-  print('\n***************************************')
-  print('*            GAME OVER!               *')
-  print('***************************************\n')
+      compareGuess(higher)
+      score = updateScore(score, secret_num, guess)
+    attempt_num += 1
+  if (not right):
+    print_loser_sreen(secret_num)
+    score -= 500
+  print(f"Sua pontuação foi {score}.\n")
+  print("\n***************************************")
+  print("*            GAME OVER!               *")
+  print("***************************************\n")
 
-if __name__ == '__main__': play()
+if __name__ == "__main__": play()
