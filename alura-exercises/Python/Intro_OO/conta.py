284959d293b2c687d56859f0d341f9e75d6dba4f
rach-vp <picanco.raquel@gmail.com>
2020-12-21 11:18:39 -0400

    Cria classe Account

--- alura-exercises/Python/Intro_OO/conta.py
+++ alura-exercises/Python/Intro_OO/conta.py
@@ -1,17 +1,24 @@
-def new_account(id, holder, balance, limit):
-    account = {
-      "id": id,
-      "holder": holder,
-      "balance": balance,
-      "limit": limit,
-    }
-    return account
+# def new_account(id, holder, balance, limit):
+#     account = {
+#       "id": id,
+#       "holder": holder,
+#       "balance": balance,
+#       "limit": limit,
+#     }
+#     return account
 
-def deposit(account, value):
-    account["balance"] += value
+# def deposit(account, value):
+#     account["balance"] += value
 
-def withdraw(account, value):
-    account["balance"] -= value
+# def withdraw(account, value):
+#     account["balance"] -= value
 
-def statement(account):
-    print(f"Saldo {account.balance}")
+# def statement(account):
+#     print(f"Saldo {account.balance}")
+
+class Account:
+  def __init__(self, id,holder, balance, limit):
+    self.id = id
+    self.holder = holder
+    self.balance = balance
+    self.limit = limit
