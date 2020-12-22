a8750300d6d0cb3365f4f6b057bf4d281b8f2f2e
rach-vp <picanco.raquel@gmail.com>
2020-12-22 11:35:13 -0400

    Adiciona funções encapsuladas

--- alura-exercises/Python/Intro_OO/conta.py
+++ alura-exercises/Python/Intro_OO/conta.py
@@ -17,14 +17,22 @@
 #     print(f"Saldo {account.balance}")
 
 class Account:
-  def __init__(self, id, holder, balance, limit):
-    self.id = id
-    self.holder = holder
-    self.balance = balance
-    self.limit = limit
+  def __init__(self, id, holder, balance, limit = 1000):
+    self.__id = id
+    self.__holder = holder
+    self.__balance = balance
+    self.__limit = limit
   def deposit(self, value):
-    self.balance += value
+    self.__balance += value
   def withdraw(self, value):
-    self.balance -= value
+    self.__balance -= value
   def statement(self):
-    print(f"Saldo da conta {self.id}: R${self.balance:.2f}")
+    print(f"Saldo da conta {self.__id}: R${self.__balance:.2f}")
+  def transfer(self, value, target):
+    self.withdraw(value)
+    target.withdraw(value)
+  def is_overdue(self):
+    overdue = False
+    if (self.__balance < 0):
+      overdue = True
+    return overdue
