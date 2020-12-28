f8a8bbf689584c6056444a15c2551ede7e0f87ec
rach-vp <picanco.raquel@gmail.com>
2020-12-28 08:48:25 -0400

    Implementa extração de argumentos da URL

--- alura-exercises/Python/Strings_Manipulation/URL_arg_extract.py
+++ alura-exercises/Python/Strings_Manipulation/URL_arg_extract.py
@@ -1,8 +1,11 @@
+# url = "www.bytebank.com.br/cambio?valor=1500&moedaOrigem=real&moedaDestino=dolar"
+
 class URL_arg_extract:
   def __init__(self, url):
     if self.is_url:
       self._url = url
       self._exchange_request = url[url.find("?") + 1:]
+      self._arg_array = self._exchange_request.split("&")
     else:
       raise LookupError("URL inválida")
 
@@ -13,3 +16,18 @@ class URL_arg_extract:
 
   def __str__(self):
     return self._exchange_request
+
+  def extract_value(self):
+    value = str(self._arg_array[0])
+    value = value[value.find("=") + 1:]
+    return value
+
+  def extract_origin_currency(self):
+    origin_currency = str(self._arg_array[1])
+    origin_currency = origin_currency[origin_currency.find("=") + 1:]
+    return origin_currency
+
+  def extract_destination_currency(self):
+    destination_currency = str(self._arg_array[2])
+    destination_currency = destination_currency[destination_currency.find("=") + 1:]
+    return destination_currency
--- alura-exercises/Python/Strings_Manipulation/main.py
+++ alura-exercises/Python/Strings_Manipulation/main.py
@@ -2,5 +2,10 @@ from URL_arg_extract import URL_arg_extract
 
 url = "www.bytebank.com.br/cambio?valor=1500&moedaOrigem=real&moedaDestino=dolar"
 
-exchange = URL_arg_extract(url)
-print(exchange)
+exchange_data = URL_arg_extract(url)
+
+# exchange_request = url[url.find("?") + 1:]
+
+print(exchange_data.extract_value())
+print(exchange_data.extract_destination_currency())
+print(exchange_data.extract_origin_currency())
