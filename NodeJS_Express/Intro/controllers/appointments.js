8b0714d37bae2bfbd8325a3c74ad67c519caeaa4
Raquel Picanço <picanco.raquel@gmail.com>
2021-01-27 18:37:21 -0400

    Add body-parser

--- NodeJS_Express/Intro/controllers/appointments.js
+++ NodeJS_Express/Intro/controllers/appointments.js
@@ -1,1 +1,7 @@
-module.exports = app => app.get('/appointments', (req, res) => res.send('You are getting the appointments rout'));
+module.exports = app => {
+  app.get('/appointments', (req, res) => res.send('You are at appointments rout using GET'));
+  app.post('/appointments', (req, res) => {
+    console.log(req.body);
+    res.send('You are at appointments rout using POST');
+  });
+}
