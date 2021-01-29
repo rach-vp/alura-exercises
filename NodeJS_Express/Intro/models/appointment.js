83d3a105baf419476dd31006238a0b3a7edc1057
Raquel Picanço <picanco.raquel@gmail.com>
2021-01-28 20:23:02 -0400

    Implement appointment date

--- NodeJS_Express/Intro/models/appointment.js
+++ NodeJS_Express/Intro/models/appointment.js
@@ -1,9 +1,13 @@
-const connection = require("../infra/connection");
+const connection = require('../infra/connection');
+const moment = require('moment');
 
 class Appointment {
   add(appointment) {
+    const dataCreation = moment().format('YYYY-MM-DD HH:MM:SS');
+    const data = moment(appointment.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+    const appointmentFullRecord = {...appointment, dataCreation, data};
     const sql = 'INSERT INTO Appointments SET ?';
-    connection.query(sql, appointment, (erro, result) => erro ? console.log(erro) : console.log(result));
+    connection.query(sql, appointmentFullRecord, (erro, result) => erro ? console.log(erro) : console.log(result));
   }
 }
 
