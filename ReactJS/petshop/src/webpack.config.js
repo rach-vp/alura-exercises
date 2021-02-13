0fdbd913ad2fde6ac9a43dca02bf7466c2196afa
Raquel Picanço <picanco.raquel@gmail.com>
2021-02-12 20:04:05 -0400

    Generate bundle, create devServer and install HTML plugin

---
+++ ReactJS/petshop/src/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    open: true,
+    contentBase: 'dist',
+  },
+  entry: './componentes/lista/listagem-cliente.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+        template: './clientes.html',
+        filename: 'index.html',
+      })
+  ],
+}
