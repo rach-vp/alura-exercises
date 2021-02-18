32ba8a72bf6d31f25df3cf15cadc52ab80304cf0
Raquel Picanço <picanco.raquel@gmail.com>
2021-02-17 20:04:00 -0400

    Includes bootstrap in the webpack bundle

--- Front-End/VueJS/alura-frame-webpack/client/webpack.config.js
+++ Front-End/VueJS/alura-frame-webpack/client/webpack.config.js
@@ -1,25 +1,46 @@
-const path =  require('path');
-const babiliWebpackPlugin = require('babili-webpack-plugin');
+const path = require("path");
+const babiliWebpackPlugin = require("babili-webpack-plugin");
 
 const plugins = [];
 
-if (process.env.NODE_ENV === 'production') plugins.push(new babiliWebpackPlugin());
+if (process.env.NODE_ENV === "production")
+  plugins.push(new babiliWebpackPlugin());
 
 module.exports = {
-  entry: './app-src/app.js',
+  entry: "./app-src/app.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist',
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "dist",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: { loader: 'babel-loader' },
-      }
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff",
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=application/octet-stream",
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader",
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml",
+      },
     ],
   },
   plugins,
-}
+};
