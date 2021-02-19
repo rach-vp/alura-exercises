1e0b828b70ec8f913b5f2a02c43977eb55136e63
Raquel Picanço <picanco.raquel@gmail.com>
2021-02-19 19:29:24 -0400

    Implement filter by card title

--- Front-End/VueJS/alurapic/src/App.vue
+++ Front-End/VueJS/alurapic/src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <main class="main-content">
     <h1 class="centered">{{ title }}</h1>
+    <input
+      type="search"
+      class="filter"
+      placeholder="Filter by title"
+      v-on:input="filterQuery = new RegExp($event.target.value.trim(), 'i')"
+    />
     <ul class="list-pictures">
-      <li class="list-pictures__item" v-for="{ url, titulo } of pictures" :key="titulo">
+      <li
+        class="list-pictures__item"
+        v-for="{ url, titulo } of filteredCards"
+        :key="titulo"
+      >
         <card :title="titulo" :src="url" :alt="titulo" />
       </li>
     </ul>
@@ -10,42 +20,63 @@
 </template>
 
 <script>
-import Card from './components/shared/Card/Card';
+import Card from "./components/shared/Card/Card";
 
 export default {
   components: {
-    'card': Card,
+    card: Card,
   },
-  data () {
+  data() {
     return {
       title: "AluraPic",
       pictures: [],
-    }
+      filterQuery: "",
+    };
   },
-  created () {
-    this.$http.get('http://localhost:3000/v1/fotos')
-      .then(res => res.json())
-      .then(res => this.pictures = res, err => console.log(err));
-  }
-}
+  computed: {
+    filteredCards() {
+      let visibleCards = this.pictures;
+      if (this.filterQuery) {
+        visibleCards = this.pictures.filter(({ titulo }) =>
+          titulo.match(this.filterQuery)
+        );
+      }
+      return visibleCards;
+    },
+  },
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      .then((res) => res.json())
+      .then(
+        (res) => (this.pictures = res),
+        (err) => console.log(err)
+      );
+  },
+};
 </script>
 
 <style>
-  .centered {
-    text-align: center;
-  }
+.centered {
+  text-align: center;
+}
 
-  .main-content {
-    font-family: Helvetica, sans-serif;
-    margin: 0 auto;
-    width: 96%;
-  }
+.main-content {
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+}
 
-  .list-pictures {
-    list-style: none;
-  }
+.list-pictures {
+  list-style: none;
+}
 
-  .list-pictures__item {
-    display: inline-block;
-  }
+.list-pictures__item {
+  display: inline-block;
+}
+
+.filter {
+  display: block;
+  width: 100%;
+}
 </style>
