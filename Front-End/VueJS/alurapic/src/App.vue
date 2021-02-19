1ea8595215268ddfb66a8592c278a0cd659943f9
Raquel Picanço <picanco.raquel@gmail.com>
2021-02-19 13:33:26 -0400

    Create component Card

--- Front-End/VueJS/alurapic/src/App.vue
+++ Front-End/VueJS/alurapic/src/App.vue
@@ -1,12 +1,21 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <img v-for="pic in pictures" :key="pic.titulo" :src="pic.url" :alt="pic.titulo">
-  </div>
+  <main class="main-content">
+    <h1 class="centered">{{ title }}</h1>
+    <ul class="list-pictures">
+      <li class="list-pictures__item" v-for="{ url, titulo } of pictures" :key="titulo">
+        <card :title="titulo" :src="url" :alt="titulo" />
+      </li>
+    </ul>
+  </main>
 </template>
 
 <script>
+import Card from './components/shared/Card/Card';
+
 export default {
+  components: {
+    'card': Card,
+  },
   data () {
     return {
       title: "AluraPic",
@@ -20,3 +29,23 @@ export default {
   }
 }
 </script>
+
+<style>
+  .centered {
+    text-align: center;
+  }
+
+  .main-content {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .list-pictures {
+    list-style: none;
+  }
+
+  .list-pictures__item {
+    display: inline-block;
+  }
+</style>
