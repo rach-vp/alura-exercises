9e4ed4ebefd318d2042ddeb455abf0ee3c351e79
Raquel Picanço <picanco.raquel@gmail.com>
2021-02-19 21:54:40 -0400

    .

---
+++ Front-End/VueJS/alurapic/src/components/shared/Nav/Nav.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav>
+    <li :key="path" v-for="{ path, title } in routes">
+      <router-link :to="path ? path : '/'">{{ title }}</router-link>
+    </li>
+  </nav>
+</template>
+
+<script>
+import routes from "../../../routes";
+
+export default {
+  data() {
+    return { routes };
+  },
+};
+</script>
---
+++ Front-End/VueJS/alurapic/src/routes.js
@@ -0,0 +1,9 @@
+import Home from "./components/Home";
+import Register from "./components/Register";
+
+const routes = [
+  { path: "", component: Home, title: "Home" },
+  { path: "/register", component: Register, title: "Register" }
+];
+
+export default routes;
