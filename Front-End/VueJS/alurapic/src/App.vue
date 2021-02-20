4fb2d2344770d9d2570e7b5fac828ebe55bb440f
Raquel Picanço <picanco.raquel@gmail.com>
2021-02-19 21:50:32 -0400

    Create component Nav

--- Front-End/VueJS/alurapic/src/App.vue
+++ Front-End/VueJS/alurapic/src/App.vue
@@ -1,14 +1,32 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <nav-bar />
+    <main>
+      <transition name="main">
+        <router-view></router-view>
+      </transition>
+    </main>
+  </div>
 </template>
 
 <script>
+import Nav from './components/shared/Nav';
 
 export default {
-
+  components: {
+    'nav-bar': Nav,
+  }
 };
 </script>
 
 <style>
+  .main-enter,
+  .main-leave-active {
+    opacity: 0;
+  }
 
+  .main-leave-active,
+  .main-enter-active {
+    transition: opacity 0.3s;
+  }
 </style>
--- Front-End/VueJS/alurapic/src/components/shared/Card/Card.vue
+++ Front-End/VueJS/alurapic/src/components/shared/Card/Card.vue
@@ -3,7 +3,7 @@
     <h2 class="card__title" @dblclick="visible = !visible">{{ title }}</h2>
     <transition name="card__body--fade">
       <div class="card__body" v-show="visible">
-        <img :src="src" :alt="alt" class="card__body--picture">
+        <img :src="src" :alt="alt" class="card__body--picture" />
       </div>
     </transition>
   </section>
@@ -11,51 +11,62 @@
 
 <script>
 export default {
-  props: ['title', 'src', 'alt'],
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    src: {
+      type: String,
+      required: true,
+    },
+    alt: {
+      type: String,
+      required: true,
+    },
+  },
   data() {
     return {
       visible: true,
-    }
-  }
-}
+    };
+  },
+};
 </script>
 
 <style scoped>
-   .card {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-    user-select: none;
-  }
+.card {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+  user-select: none;
+}
 
-  .card__title {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
-  }
+.card__title {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
 
-  .card__body--picture {
-    width: 100%;
-  }
+.card__body--picture {
+  width: 100%;
+}
 
-  .card__body--fade-enter,
-  .card__body--fade-leave-active {
-    opacity: 0;
-  }
+.card__body--fade-enter,
+.card__body--fade-leave-active {
+  opacity: 0;
+}
 
-  .card__body--fade-leave-active,
-  .card__body--fade-enter-active {
-    transition: opacity 0.5s;
-  }
-
-
+.card__body--fade-leave-active,
+.card__body--fade-enter-active {
+  transition: opacity 0.5s;
+}
 </style>
