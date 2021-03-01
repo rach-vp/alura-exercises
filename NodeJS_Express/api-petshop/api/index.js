dd4abfb3f30287b1abb4af83bddc521e25229e33
Raquel Picanço <picanco.raquel@gmail.com>
2021-03-01 19:46:24 -0400

    Create connection with database

--- NodeJS_Express/api-petshop/api/index.js
+++ NodeJS_Express/api-petshop/api/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const config = require('config');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || config.get(api.port);
 app.listen(port, () => console.log(`Listening at port ${port}`));
