700badf5d6fa2a2e95f7f1a8896d8e6208cee827
Raquel Picanço <picanco.raquel@gmail.com>
2021-04-08 19:54:53 -0400

    Create models for classes, registrations and levels

---
+++ NodeJS_Express/orm-sequelize/api/migrations/20210408235258-create-classes.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Classes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      date_beginning: {
+        type: Sequelize.DATEONLY
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Classes');
+  }
+};
