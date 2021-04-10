676e72d3f4bcb8809e1d776d8fd2eda196521147
Raquel Picanço <picanco.raquel@gmail.com>
2021-04-10 12:48:09 -0400

    Implment refresh token authetication

--- NodeJS_Express/blog-do-codigo/src/usuarios/middlewares-auth.js
+++ NodeJS_Express/blog-do-codigo/src/usuarios/middlewares-auth.js
@@ -1,4 +1,22 @@
 const passport = require('passport');
+const Usuario = require('./usuarios-modelo');
+const  { InvalidArgumentError } = require('../erros');
+const allowListRefreshToken = require('../../redis/allowlist-refresh-token');
+
+const verificaRefreshToken = async (refreshToken) => {
+  if (!refreshToken) {
+    throw new InvalidArgumentError('Refresh token não enviado');
+  }
+  const id = await allowListRefreshToken.buscaValor(refreshToken);
+  if (!id) {
+    throw new InvalidArgumentError('Refresh token inválido');
+  }
+  return id;
+};
+
+const invalidaRefreshToken = async (refreshToken) => {
+  await allowListRefreshToken.delete(refreshToken);
+};
 
 module.exports = {
   local: (req, res, next) => {
@@ -44,5 +62,19 @@ module.exports = {
         return next();
       },
     )(req, res, next);
-  }
+  },
+  refresh: async (req, res, next) => {
+    try {
+      const { refreshToken } = req.body;
+      const id = await verificaRefreshToken(refreshToken);
+      await invalidaRefreshToken(refreshToken);
+      req.user = Usuario.buscaPorId(id);
+      return next();
+    } catch (err) {
+      if (err.name === 'InvalidArgumentError') {
+        return res.status(401).json({ error: err.message });
+      }
+      return res.status(500).json({ error: err.message });
+    }
+  },
 };
