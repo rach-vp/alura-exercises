5d83ace131f728dca9d8bace0ab7abe708530d77
Raquel Picanço <picanco.raquel@gmail.com>
2021-05-19 21:56:52 -0400

    Create connection with PG

--- Django/alura_recipes/alura_recipes/settings.py
+++ Django/alura_recipes/alura_recipes/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,8 +82,11 @@ WSGI_APPLICATION = 'alura_recipes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_recipes',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('PG_PASSWORD'),
+        'HOST': 'localhost',
     }
 }
 
--- Django/alura_recipes/recipes/views.py
+++ Django/alura_recipes/recipes/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
 def index(req):
-  return render(req, 'index.html')
+  recipes = {
+    1: 'Lasagna',
+    2: 'Tomato Soupe',
+    3: 'Ice Cream',
+    4: 'Brigadeiro',
+  }
+  data = { 'recipes': recipes }
+  return render(req, 'index.html', data)
 
 def recipe(req):
   return render(req, 'receita.html')
