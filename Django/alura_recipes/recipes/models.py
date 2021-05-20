9f817baedfb092afb7ed82c4d9cd7c775f4d6c32
Raquel Picanço <picanco.raquel@gmail.com>
2021-05-19 22:06:54 -0400

    Create model recipes

--- Django/alura_recipes/recipes/models.py
+++ Django/alura_recipes/recipes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Recipe(models.Model):
+  recipe_name = models.CharField(max_length=200)
+  ingredients = models.TextField()
+  instructions = models.TextField()
+  time_prepare = models.IntegerField()
+  portions = models.CharField(max_length=100)
+  category = models.CharField(max_length=100)
+  created_at = models.DateTimeField(default=datetime.now, blank = True)
