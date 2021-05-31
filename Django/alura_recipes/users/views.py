460a4727122fd06bca0914e532569f390eb0f2b6
Raquel Picanço <picanco.raquel@gmail.com>
2021-05-31 16:24:04 -0400

    Implement user creation

--- Django/alura_recipes/users/views.py
+++ Django/alura_recipes/users/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 def login(request):
   return render(request, 'users/login.html')
@@ -7,6 +8,33 @@ def logout(request):
   pass
 
 def register(request):
+  if request.method == 'POST':
+    form_data = {
+      'Nome completo': request.POST['nome'].strip(),
+      'Email': request.POST['email'].strip(),
+      'Senha': request.POST['password'].strip(),
+      'Confirmação de senha': request.POST['password2'].strip(),
+    }
+
+    for data in form_data:
+      if not form_data[data]:
+        print(f'Field "{data}" must be filled')
+        return redirect('register')
+    if User.objects.filter(email=form_data['Email']).exists():
+      print('User already exists')
+      return redirect('register')
+    if not form_data['Senha'] == form_data['Confirmação de senha']:
+      print('Passwords must match')
+      return redirect('register')
+
+    user = User.objects.create_user(
+      username=form_data['Nome completo'],
+      email=form_data['Email'],
+      password=form_data['Senha'],
+    )
+    user.save()
+    print('User successfully registered')
+    return redirect('login')
   return render(request, 'users/register.html')
 
 def dashboard(request):
