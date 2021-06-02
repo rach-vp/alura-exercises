59848e849ac1affe9d106e1d604b7bd4ad9c2c05
Raquel Picanço <picanco.raquel@gmail.com>
2021-06-01 22:03:45 -0400

    implement recipe creation

--- Django/alura_recipes/users/views.py
+++ Django/alura_recipes/users/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import auth
+from recipes.models import Recipe
 
 def login(request):
   if request.method == 'POST':
@@ -53,8 +54,35 @@ def register(request):
 
 def dashboard(request):
   if request.user.is_authenticated:
-    return render(request, 'users/dashboard.html')
+    recipes_data = Recipe.objects.order_by('-created_at').filter(author=request.user.id)
+    recipes = {
+      'recipes': recipes_data,
+    }
+    return render(request, 'users/dashboard.html', recipes)
   return redirect('login')
 
 def create_recipe(request):
+  if request.method == 'POST':
+    recipe_data = {
+      'Nome da receita': request.POST['nome_receita'].strip(),
+      'Ingredientes': request.POST['ingredientes'].strip(),
+      'Modo de preparo': request.POST['modo_preparo'].strip(),
+      'Tempo de preparo': request.POST['tempo_preparo'].strip(),
+      'Rendimento': request.POST['rendimento'].strip(),
+      'Categoria': request.POST['categoria'].strip(),
+      'Imagem': request.FILES['foto_receita'],
+      'author': get_object_or_404(User, pk=request.user.id)
+    }
+    recipe = Recipe.objects.create(
+      author=recipe_data['author'],
+      recipe_name=recipe_data['Nome da receita'],
+      picture=recipe_data['Imagem'],
+      ingredients=recipe_data['Ingredientes'],
+      instructions=recipe_data['Modo de preparo'],
+      time_prepare=recipe_data['Tempo de preparo'],
+      portions=recipe_data['Rendimento'],
+      category=recipe_data['Categoria'],
+    )
+    recipe.save()
+    return redirect('dashboard')
   return render(request, 'users/create_recipe.html')
