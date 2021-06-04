a2f691983973499cc2dbd58ed46d744f6a84b304
Raquel Picanço <picanco.raquel@gmail.com>
2021-06-04 17:08:17 -0400

    Implement fields validation

--- Django/setup/passagens/forms.py
+++ Django/setup/passagens/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from tempus_dominus.widgets import DatePicker
 from datetime import datetime
 from passagens.classe_viagem import classes
+from passagens.validation import *
 
 class PassagensForm(forms.Form):
   origem = forms.CharField(label='Origem', max_length=100)
@@ -17,3 +18,23 @@ class PassagensForm(forms.Form):
     required=False,
   )
   email = forms.EmailField(label='Email')
+
+  def clean(self):
+    origem = self.cleaned_data.get('origem')
+    destino = self.cleaned_data.get('destino')
+    data_ida = self.cleaned_data.get('data_ida')
+    data_volta = self.cleaned_data.get('data_volta')
+    data_pesquisa = self.cleaned_data.get('data_pesquisa')
+    lista_erros = {}
+
+    campo_com_numero(origem, 'origem', lista_erros)
+    campo_com_numero(destino, 'destino', lista_erros)
+    origem_destino_iguais(origem, destino, lista_erros)
+    data_ida_maior_data_volta(data_ida, data_volta, lista_erros)
+    data_ida_menor_hoje(data_ida, data_pesquisa, lista_erros)
+
+    if lista_erros is not None:
+      for erro in lista_erros:
+        mensagem_erro = lista_erros[erro]
+        self.add_error(erro, mensagem_erro)
+    return self.cleaned_data
--- Django/setup/passagens/views.py
+++ Django/setup/passagens/views.py
@@ -3,10 +3,14 @@ from passagens.forms import PassagensForm
 
 def index(request):
   passagens_form = PassagensForm()
-  context = { 'passagens_form': passagens_form }
+  context = { 'form': passagens_form }
   return render(request, 'index.html', context)
 
 def consulta(request):
   consulta_info = PassagensForm(request.POST)
-  context = { 'info': consulta_info }
-  return render(request, 'consulta.html', context)
+  if consulta_info.is_valid():
+    context = { 'info': consulta_info }
+    return render(request, 'consulta.html', context)
+  else:
+    context = { 'form': consulta_info }
+    return render(request, 'index.html', context)
