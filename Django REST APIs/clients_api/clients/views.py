1701b811f7c3e31cde1005352d9b2ce58e549f1c
Raquel Picanço <picanco.raquel@gmail.com>
2021-06-10 08:03:38 -0400

    Include filters

--- Django REST APIs/clients_api/clients/views.py
+++ Django REST APIs/clients_api/clients/views.py
@@ -7,5 +7,11 @@ class ClientViewSet(viewsets.ModelViewSet):
   """Display clients"""
   queryset = Client.objects.all()
   serializer_class = ClientSerializer
-  filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+  filter_backends = [
+    DjangoFilterBackend,
+    filters.OrderingFilter,
+    filters.SearchFilter,
+  ]
   ordering_fields = ['name']
+  search_fields = ['name', 'cpf']
+  filterset_fields = ['active']
