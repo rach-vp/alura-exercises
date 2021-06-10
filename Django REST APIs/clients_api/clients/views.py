4ba81cd20ac4425530546e20ad423016d9ba2826
Raquel Picanço <picanco.raquel@gmail.com>
2021-06-10 10:57:46 -0400

    add permissions and authentications

--- Django REST APIs/clients_api/clients/views.py
+++ Django REST APIs/clients_api/clients/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets, filters
 from clients.models import Client
 from clients.serializers import ClientSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class ClientViewSet(viewsets.ModelViewSet):
   """Display clients"""
@@ -15,3 +17,6 @@ class ClientViewSet(viewsets.ModelViewSet):
   ordering_fields = ['name']
   search_fields = ['name', 'cpf']
   filterset_fields = ['active']
+
+  authentication_classes = [BasicAuthentication]
+  permission_classes = [IsAuthenticated]
