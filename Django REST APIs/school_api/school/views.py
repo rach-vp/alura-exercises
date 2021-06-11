5dd50a041e90b0e97245496a2ef0f8af659e981f
Raquel Picanço <picanco.raquel@gmail.com>
2021-06-11 11:40:44 -0400

    create example of header loction

--- Django REST APIs/school_api/school/views.py
+++ Django REST APIs/school_api/school/views.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, status
 from school.models import Student, Course,Enrollment
 from school.serializer import *
+from rest_framework.response import Response
 
 class StudentsViewSet(viewsets.ModelViewSet):
   """Display all students"""
@@ -13,6 +14,15 @@ class CourseViewSet(viewsets.ModelViewSet):
   """Display all courses"""
   queryset = Course.objects.all()
   serializer_class = CourseSerializer
+
+  def create(self, request):
+    serializer = self.serializer_class(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      response = Response(serializer.data, status=status.HTTP_201_CREATED)
+      id = str(serializer.data['id'])
+      response['Location'] = request.build_absolute_uri() + id
+      return response
 
 class EnrollmentViewSet(viewsets.ModelViewSet):
   """Display all enrollments"""
