c90f5baa2f933500a0b1e4e925f21ef301b04c8a
Raquel Picanço <picanco.raquel@gmail.com>
2021-06-17 07:59:40 -0400

    Implement static files upload

--- Django REST APIs/school_api/school/models.py
+++ Django REST APIs/school_api/school/models.py
@@ -6,6 +6,7 @@ class Student(models.Model):
   cpf = models.CharField(max_length=11, unique=True, blank=False)
   birth_date = models.DateField()
   phone_number = models.CharField(max_length=11, default="")
+  picture = models.ImageField(blank=True)
 
   def __str__(self):
     return self.name
--- Django REST APIs/school_api/setup/urls.py
+++ Django REST APIs/school_api/setup/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from school import views as school_viewsets
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register('students', school_viewsets.StudentsViewSet, basename='Students')
@@ -13,4 +15,4 @@ urlpatterns = [
     path('', include(router.urls)),
     path('students/<int:pk>/enrollments', school_viewsets.EnrollmentPerStudent.as_view()),
     path('courses/<int:pk>/enrollments', school_viewsets.StudentsEnrolledInCourse.as_view()),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
