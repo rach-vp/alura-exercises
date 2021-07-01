9df384fa339c1e802c4772f7e63a485c74dc788e
Raquel Picanço <picanco.raquel@gmail.com>
2021-07-01 12:01:42 -0400

    comitão bomba @_@

--- Django/tdd_animal_search/animals/models.py
+++ Django/tdd_animal_search/animals/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Animal(models.Model):
+  name = models.CharField(max_length=50)
+  predator = models.BooleanField(default=False)
+  poisonous = models.BooleanField(default=False)
+  domestic = models.BooleanField(default=False)
+
+  def __str__(self):
+    return self.name
--- Django/tdd_animal_search/setup/tests.py
+++ Django/tdd_animal_search/setup/tests.py
@@ -1,10 +1,17 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 import os
+from animals.models import Animal
 
 class AnimalsTestCase(LiveServerTestCase):
   def setUp(self):
     self.browser = webdriver.Chrome(executable_path=os.getenv('CHROMEDRIVER_PATH'))
+    self.animal = Animal.objects.create(
+      name="Lion",
+      predator=True,
+      poisonous=False,
+      domestic=False,
+    )
 
   def tearDown(self):
     self.browser.quit()
@@ -16,10 +23,20 @@ class AnimalsTestCase(LiveServerTestCase):
     # Vini wishes to find an animal to adopt.
     # He finds Animal Search and decides to use the site
     home_page = self.browser.get(self.live_server_url + '/')
+
     # because he sees "Animal Search" in the menu
     brand_element = self.browser.find_element_by_css_selector('.navbar')
     self.assertEqual('Animal Search', brand_element.text)
+
     # He sees a fields to search for na animal
+    search_animal_input = self.browser.find_element_by_css_selector('input#search-animal')
+    self.assertEqual(search_animal_input.get_attribute('placeholder'), 'Example: lion')
+
     # He search for "lion" and clicks the button "search"
+    search_animal_input.send_keys('lion')
+    self.browser.find_element_by_css_selector('form button').click()
     # The site shows 4 features of the searched animal
+    features = self.browser.find_elements_by_css_selector('.result-description')
+    self.assertGreater(len(features), 3)
+
     # Vini decides not to adopt a lion
