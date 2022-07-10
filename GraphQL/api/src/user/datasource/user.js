dddc6284ec30fc82ff5547cf7e6e256128cd1e4c
Raquel Picanço <picanco.raquel@gmail.com>
2022-07-10 09:38:41 -0400

    feat: Complete CRUD for users

--- GraphQL/api/src/user/datasource/user.js
+++ GraphQL/api/src/user/datasource/user.js
@@ -17,6 +17,35 @@ class UsersAPI extends RESTDataSource {
     const user = await this.get(`/users/${id}`);
     return { ...user, role: await this.get(`/roles/${user.role}`) }
   }
+
+  async addUser(data) {
+    const { role: roleStr, ...userData } = data;
+    const [role] = await this.get(`/roles?type=${roleStr}`);
+    return this.post(
+      '/users',
+      { id: await this.autoincrementId(), role: role.id, ...userData },
+    );
+  }
+
+  async updateUser(data) {
+    const { role: roleStr, ...userData } = data;
+    const [role] = await this.get(`/roles?type=${roleStr}`);
+    return this.patch(
+      `/users/${userData.id}`,
+      { role: role.id, ...userData },
+    );
+  }
+
+  async removeUser(id) {
+    await this.delete(`/users/${id}`);
+    return id;
+  }
+
+  async autoincrementId() {
+    const users = await this.get('/users');
+    const { id } = users.at(-1);
+    return id + 1;
+  }
 }
 
 module.exports = UsersAPI;
--- GraphQL/api/src/user/resolvers/userResolvers.js
+++ GraphQL/api/src/user/resolvers/userResolvers.js
@@ -3,4 +3,9 @@ module.exports = {
     users: (_root, _args, { dataSources }) => dataSources.usersAPI.getUsers(),
     userById: (_root, { id }, { dataSources }) => dataSources.usersAPI.getUserById(id),
   },
+  Mutation: {
+    addUser: (_root, user, { dataSources }) => dataSources.usersAPI.addUser(user),
+    updateUser: (_root, data, { dataSources }) => dataSources.usersAPI.updateUser(data),
+    removeUser: (_root, { id }, { dataSources }) => dataSources.usersAPI.removeUser(id),
+  },
 };
