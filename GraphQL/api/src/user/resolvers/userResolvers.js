b439875eddca45ad2f94c3be6c11e217694b9d67
Raquel Picanço <picanco.raquel@gmail.com>
2022-07-10 10:20:10 -0400

    feat: Create sclar type for dates

--- GraphQL/api/src/user/resolvers/userResolvers.js
+++ GraphQL/api/src/user/resolvers/userResolvers.js
@@ -1,4 +1,13 @@
+const { GraphQLScalarType } = require('graphql');
+
 module.exports = {
+  DateTime: new GraphQLScalarType({
+    name: 'DateTime',
+    description: 'String of datetime ISO-8601',
+    serialize: (value) => value.toISOString(),
+    parseValue: (value) => new Date(value),
+    parseLiteral: (ast) => new Date(ast.value),
+  }),
   Query: {
     users: (_root, _args, { dataSources }) => dataSources.usersAPI.getUsers(),
     userById: (_root, { id }, { dataSources }) => dataSources.usersAPI.getUserById(id),
