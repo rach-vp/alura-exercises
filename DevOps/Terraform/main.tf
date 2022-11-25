19531b2bce7303f06d7466453bc8dffdae895f7e
Raquel Picanço <picanco.raquel@gmail.com>
2022-11-25 09:38:50 -0400

    feat: Create s3 bucket

--- DevOps/Terraform/main.tf
+++ DevOps/Terraform/main.tf
@@ -33,5 +33,31 @@ resource "aws_instance" "dev" {
   tags          = {
     "Name" = "dev-${count.index}"
   }
-  vpc_security_group_ids = ["sg-05b8fe4e6097e24db"]
+  vpc_security_group_ids = ["${aws_security_group.ssh_access.id}"]
+}
+
+resource "aws_s3_bucket" "dev3-s3" {
+  bucket = "terraform-aws-alura-dev3"
+
+  tags = {
+    Name = "terraform-aws-alura-dev3"
+  }
+}
+
+resource "aws_s3_bucket_acl" "dev3-s3-acl" {
+  bucket = aws_s3_bucket.dev3-s3.id
+  acl    = "private"
+}
+
+resource "aws_instance" "dev3" {
+  ami           = "ami-0b0dcb5067f052a63"
+  instance_type = "t2.micro"
+  key_name      = "terraform-aws-alura"
+  tags          = {
+    "Name" = "dev-3"
+  }
+  vpc_security_group_ids = ["${aws_security_group.ssh_access.id}"]
+  depends_on = [
+    aws_s3_bucket.dev3-s3
+  ]
 }
