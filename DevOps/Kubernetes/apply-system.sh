f061c1e901ccb95c9cc2d34b4be8afa772d2c9cc
Raquel Picanço <picanco.raquel@gmail.com>
2022-12-08 15:29:55 -0400

    chore: Organize files

--- DevOps/Kubernetes/apply-system.sh
+++ DevOps/Kubernetes/apply-system.sh
@@ -3,16 +3,16 @@
 kubectl config use-context docker-desktop
 
 echo '--> Creating DB'
-kubectl apply -f ./news-db-configmap.yaml
-kubectl apply -f ./news-db.yaml
-kubectl apply -f ./news-db-svc.yaml
+kubectl apply -f ./configMaps/news-db-configmap.yaml
+kubectl apply -f ./pods/news-db.yaml
+kubectl apply -f ./services/news-db-svc.yaml
 
 echo '--> Creating news system'
-kubectl apply -f ./news-system-configmap.yaml
-kubectl apply -f ./news-system.yaml
-kubectl apply -f ./news-system-svc.yaml
+kubectl apply -f ./configMaps/news-system-configmap.yaml
+kubectl apply -f ./pods/news-system.yaml
+kubectl apply -f ./services/news-system-svc.yaml
 
 echo '--> Creating portal'
-kubectl apply -f ./news-portal-configmap.yaml
-kubectl apply -f ./news-portal.yaml
-kubectl apply -f ./news-portal-svc.yaml
+kubectl apply -f ./configMaps/news-portal-configmap.yaml
+kubectl apply -f ./pods/news-portal.yaml
+kubectl apply -f ./services/news-portal-svc.yaml
--- DevOps/Kubernetes/cleanup.sh
+++ DevOps/Kubernetes/cleanup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 kubectl config use-context docker-desktop
-kubectl delete -f ./news-db-svc.yaml
-kubectl delete -f ./news-db.yaml
-kubectl delete -f ./news-portal-svc.yaml
-kubectl delete -f ./news-portal.yaml
-kubectl delete -f ./news-system-configmap.yaml
-kubectl delete -f ./news-system-svc.yaml
-kubectl delete -f ./news-system.yaml
-kubectl delete -f ./news-db-configmap.yaml
-kubectl delete -f ./news-system-configmap.yaml
-kubectl delete -f ./news-portal-configmap.yaml
+kubectl delete -f ./services/news-db-svc.yaml
+kubectl delete -f ./pods/news-db.yaml
+kubectl delete -f ./services/news-portal-svc.yaml
+kubectl delete -f ./pods/news-portal.yaml
+kubectl delete -f ./services/news-system-svc.yaml
+kubectl delete -f ./pods/news-system.yaml
+kubectl delete -f ./configMaps/news-db-configmap.yaml
+kubectl delete -f ./configMaps/news-system-configmap.yaml
+kubectl delete -f ./configMaps/news-portal-configmap.yaml
